123385b7c8ecc61a33ec2601587b4ee9d6984944
Sergey Ershov <ershovu@gmail.com>
2019-11-16 15:20:12 +0300

    Create backup_sql.sh

---
+++ doc/backup_sql.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mysqldump -u${MYSQL_USER} -p${MYSQL_PASSWORD} --single-transaction --databases ${MYSQL_DATABASE} | gzip -9 -c > /mysql_backup/${MYSQL_DATABASE}_date "+%Y-%m-%d".gz && echo   '  --->  Done.'
+
+X=+30
+# удаляем файлы резервных копий, кроме последних X-1
+rm -rf ls -a /mysql_backup/${MYSQL_DATABASE}_2* | sort -r | tail -n ${X}
